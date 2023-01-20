dcc972b846e3db51a1fe209a52348a2fb07ae5dc
Jatin Miglani <97083621+Jatin-Miglani@users.noreply.github.com>
2023-01-20 19:59:53 +0530

    Added 20-01-2023

---
+++ Matrices/SparseMatrix.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+class Element{
+    public:
+    int i;
+    int j;
+    int x;
+};
+class Sparse{
+    private:
+    int m;int n;int num;
+    Element *ele;
+    public:
+    Sparse(int m,int n,int num){
+        this->m=m;
+        this->n=n;
+        this->num=num;
+        ele=new Element[this->num];
+    }
+    ~Sparse(){
+        delete []ele;
+    }
+    void read(){
+        cout<<"Enter non zero elements "<<endl;
+        for(int i=0;i<num;i++){
+            cout<<"Enter row 0 to "<<m-1<<" "<<endl;
+            cin>>ele[i].i;
+            cout<<"Enter column 0 to "<<n-1<<" "<<endl;
+            cin>>ele[i].j;
+            cout<<"Enter value there "<<endl;
+            cin>>ele[i].x;
+        }
+
+    }
+    void display(){
+        int k=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(ele[k].i==i && ele[k].j==j){
+                    cout<<ele[k++].x<<" ";
+                }
+                else{
+                    cout<<"0 ";
+                }
+            }
+            cout<<endl;
+        }
+    }
+};
+int main(){
+    Sparse s1(5,5,5);
+    s1.read();
+    s1.display();
+
+    return 0;
+}
