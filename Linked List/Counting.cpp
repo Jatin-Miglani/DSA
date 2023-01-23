bed27ccc3c9914625e674f7a66c41fffd6f09929
Jatin Miglani <97083621+Jatin-Miglani@users.noreply.github.com>
2023-01-23 16:01:32 +0530

    Added 23-01-2023

---
+++ Linked List/Counting.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node *head;
+
+    void Create(){
+        int *B;
+        int n;
+        cout<<"Enter the number of elements ";
+        cin>>n;
+        B=new int[n];
+        for(int i=0;i<n;i++){
+            cin>>B[i];
+        }
+        head=new Node;
+        head->data=B[0];
+        head->next=nullptr;
+
+        Node *last;
+        last=head;
+
+        Node *temp;
+        for(int i=1;i<n;i++){
+            temp=new Node;
+            temp->data=B[i];
+            temp->next=nullptr;
+            last->next=temp;
+            last=temp;
+        }
+
+    }
+
+    // void DisplayandCount(){
+    //     Node *p=head;
+    //     int count=0;
+    //     while(p!=nullptr){
+    //         count++;
+    //         cout<<p->data<<"->";
+    //         p=p->next;
+    //     }
+    //     cout<<"/0"<<endl;
+    //     cout<<count<<endl;
+    // }
+
+    int DisplayandCount(Node *p){
+        if(p!=nullptr){
+            cout<<p->data<<"->";
+            return DisplayandCount(p->next)+1;
+        }
+        else{
+            cout<<"/0"<<endl;
+            return 0;
+        }
+    }
+};
+int main(){
+    Node n;
+    n.Create();
+    Node *b=n.head;
+    cout<<n.DisplayandCount(b);
+    return 0;
+}
