82adce591649d6b508087c9b6f525afc2ee5ede8
Jatin Miglani <97083621+Jatin-Miglani@users.noreply.github.com>
2023-01-27 11:56:40 +0530

    Added 27-01-2023

---
+++ Linked List/ReversingLinks.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node *head;
+    int count;
+
+    void Create(){
+        int n;
+        cout<<"Enter the number of elements "<<endl;
+        cin>>n;
+        head=new Node;
+        int *A=new int[n];
+        for(int i=0;i<n;i++){
+            cin>>A[i];
+        }
+        head->data=A[0];
+        head->next=nullptr;
+        Node *last=head;
+        Node *temp;
+        for(int i=1;i<n;i++){
+            temp=new Node;
+            temp->data=A[i];
+            temp->next=nullptr;
+            last->next=temp;
+            last=temp;
+        }
+        count=n;
+        delete []A;
+
+    }
+
+    void Display(){
+        Node *p=head;
+        while(p!=nullptr){
+            cout<<p->data<<"->";
+            p=p->next;
+        }
+        cout<<"/0"<<endl;
+    }
+
+    void ReverseL(){
+        Node *p=head;
+        Node *q=NULL;
+        Node *r=NULL;
+        while(p!=nullptr){
+            r=q;
+            q=p;
+            p=p->next;
+            q->next=r;
+        }
+        head=q;
+    }
+};
+int main(){
+    Node n;
+    n.Create();
+    cout<<"Simple Linked list is ";
+    n.Display();
+    cout<<endl<<"Reversed Linked list is ";
+    n.ReverseL();
+    n.Display();
+    return 0;
+}
