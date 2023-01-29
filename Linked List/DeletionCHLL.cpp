54ff00744c43ba0c2ab74ec8b4a4d6e3700f3e5d
Jatin Miglani <97083621+Jatin-Miglani@users.noreply.github.com>
2023-01-29 12:39:59 +0530

    Added 29-01-2023

---
+++ Linked List/DeletionCHLL.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node *head;
+    Node *top;
+
+    void Create(int A[],int n){
+        top=new Node;
+        head=new Node;
+        top->data=404;
+        top->next=head;
+        head->data=A[0];
+        head->next=top;
+        Node *last=head;
+        Node *temp;
+        for(int i=1;i<n;i++){
+            temp=new Node;
+            temp->data=A[i];
+            temp->next=top;
+            last->next=temp;
+            last=temp;
+        }
+    }
+
+    void Display(){
+        Node *p=top;
+        int count=0;
+        do
+        {   
+            if(count==0){
+                cout<<"|"<<p->data<<"|"<<"->";
+            }
+            else{
+                cout<<p->data<<"->";
+            }
+            p=p->next;
+            count++;
+        } while (p!=top);
+        
+        cout<<"<>"<<endl;
+    }
+
+    int Delete(int a){
+        Node *p=NULL;
+        Node *q=top;
+        Node *r=top;
+        int count=-1;
+        do
+        {
+            count++;
+            if(a==count){
+                Node *temp;
+                temp->data=q->data;
+                temp->next=nullptr;
+                p->next=q->next;
+                return 0;
+            }
+            if(q->next==top && a==count){
+                Node *temp;
+                temp->data=q->data;
+                temp->next=nullptr;
+                p->next=top;
+            }
+            p=q;
+            q=q->next;
+        } while (q!=top);
+        
+    }
+};
+int main(){
+    int A[]={23,67,12,98,26,57,45,39,76};
+    int n1=sizeof(A)/sizeof(A[0]);
+    Node n;
+    n.Create(A,n1);
+    n.Display();
+    n.Delete(1);
+    n.Display();
+    return 0;
+}
